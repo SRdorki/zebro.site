78cfb82a6de5b3b7bf2fb800317028cf8077ee7e
SRdorki <lucasmachadocosta08@gmail.com>
2026-08-15 01:35:25 -0300

    fix: allow public access to embed route

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -38,6 +38,7 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/register") &&
     !request.nextUrl.pathname.startsWith("/forgot-password") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/embed") &&
     request.nextUrl.pathname !== "/"
   ) {
     const url = request.nextUrl.clone();
